4671f236d6fdbeb019fa3369d37ff1b4380c0764
synoti21 <synoti21@naver.com>
2024-05-14 21:44:05 +0900

    ✨ Feat: example03

--- example03-if-switch-const/main.go
+++ example03-if-switch-const/main.go
@@ -3,67 +3,40 @@ package main
 import "fmt"
 
 const (
-	monday = iota + 1
-	tuesday
-	wednesday
-	thursday
-	friday
-	saturday
-	sunday
+	ADD      = "+"
+	DELETE   = "-"
+	MULTIPLE = "*"
+	DIVIDE   = "/"
 )
-
-const (
-	a = iota
-	b
-)
-
-func calc() (int, int) {
-	return 1, 2
-}
 
 func main() {
-	fmt.Println(monday)
-	fmt.Println(tuesday)
-	fmt.Println(wednesday)
-	fmt.Println(thursday)
-	fmt.Println(friday)
-	fmt.Println(saturday)
-	fmt.Println(sunday)
-	fmt.Println(a)
-	fmt.Println(b)
+	var a, b int
+	var operator string
 
-	test := true
+	fmt.Printf("첫 번째 숫자를 입력하세요: ")
+	fmt.Scanf("%d", &a)
+	fmt.Printf("두 번째 숫자를 입력하세요: ")
+	fmt.Scanf("%d", &b)
+	fmt.Printf("연산자를 입력하세요 (+, -, *, /): ")
+	fmt.Scanf("%s", &operator)
 
-	if test, test2 := calc(); test+test2 < 10 {
-		fmt.Println("test:", test)
-		fmt.Println("test2:", test2)
-		fmt.Println("test + test2 < 10")
+	if operator != ADD && operator != DELETE && operator != MULTIPLE && operator != DIVIDE {
+		fmt.Println("에러: 유효하지 않은 연산자입니다.")
+		return
 	}
 
-	fmt.Println("test:", test)
-
-	switch a := 1; {
-	case a >= 0:
-		fmt.Println("a is true")
-		fallthrough
-	case a > 200:
-		fmt.Println("a is false")
+	switch operator {
+	case ADD:
+		fmt.Printf("결과: %d\n", a+b)
+	case DELETE:
+		fmt.Printf("결과: %d\n", a-b)
+	case MULTIPLE:
+		fmt.Printf("결과: %d\n", a*b)
+	case DIVIDE:
+		if b == 0 {
+			fmt.Println("에러: 0으로 나눌 수 없습니다.")
+			return
+		}
+		fmt.Printf("결과: %f\n", float64(a)/float64(b))
 	}
-
-	stringSlice1 := []string{"1", "2", "3", "4"}
-	fmt.Println(stringSlice1[:2])
-	fmt.Println(stringSlice1[2:])
-
-	stringSlice2 := make([]string, 5, 10)
-	copy(stringSlice2, stringSlice1)
-	fmt.Println(stringSlice2[0])
-
-	stringSlice2 = append(stringSlice2, "5", "6")
-	fmt.Println(stringSlice2)
-	fmt.Println(stringSlice2[5])
-	fmt.Println(stringSlice2[6])
-	stringSlice2 = append(stringSlice2, []string{"7", "8"}...)
-	fmt.Println(stringSlice2)
-	fmt.Println(stringSlice2[7])
-	fmt.Println(stringSlice2[8])
 }
