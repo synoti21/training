4be3bd290ebbe79689e39a13e53c586cb56839e7
Ahn Jiwan <synoti21@naver.com>
2024-05-19 19:11:53 +0900

    :sparkles: feat: assign3 - use buffered channel

---
+++ example06-go-concurrency/assignment/as3.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func send(ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 1; i <= 5; i++ {
+		fmt.Printf("Sending: %d\n", i)
+		ch <- i
+	}
+	close(ch)
+}
+
+func receive(ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Received: %d\n", <-ch)
+	}
+}
+
+func main() {
+	wg := sync.WaitGroup{}
+	ch := make(chan int, 5)
+
+	wg.Add(2)
+	go send(ch, &wg)
+	go receive(ch, &wg)
+
+	wg.Wait()
+}
